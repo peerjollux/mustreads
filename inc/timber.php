0b0382e1d32790b9fc15fd4386db4e19dc614476
Jaer Pollux <jaerpollux@gmail.com>
2017-07-03 18:22:39 +0200

    Added views/template folder

---
+++ inc/timber.php
@@ -0,0 +1,27 @@
+<?php
+Timber::$dirname = array('views');
+
+class MustReadsTimber extends TimberSite {
+	function __construct() {
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		parent::__construct();
+	}
+
+	function add_to_context( $context ) {
+		$context['menu'] = new TimberMenu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	function add_to_twig( $twig ) {
+		/* this is where you can add your own functions to twig */
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		return $twig;
+	}
+}
+new MustReadsTimber();
